47446e276ffc558121ec7531865699e6b07b0698
Nathaniel Goh <gohnathaniel2000@gmail.com>
2021-05-28 21:24:23 -0700

    completed baseline training

--- utils.py
+++ utils.py
@@ -1,5 +1,16 @@
 import numpy as np
+import matplotlib.pyplot as plt
 import os
+
+import torch 
+
+device = "cuda" if torch.cuda.is_available() else "cpu"
+assert device == "cuda"  
+
+seed = 42
+np.random.seed(seed)
+torch.manual_seed(seed)
+torch.cuda.manual_seed(seed)
 
 # Change directory to wherever you store your datasets
 DATA_DIR = "/home/nathgoh/Github/CS224U-NMT/datasets"
@@ -31,9 +42,38 @@ def get_max_len_sentences(source_sentences, target_sentences, sentence_length = 
 def lookup_words(x, vocab):
     return [vocab[j] for j in x]
 
-     
 def plot_perplexity(perplexities):
     plt.title("Perplexity per Epoch")
     plt.xlabel("Epoch")
     plt.ylabel("Perplexity")
     plt.plot(perplexities)
+
+def print_examples(model, source_vocab, target_vocab, data_loader, decoder, attention = False, n = 3, EOS_INDEX = 3, max_len = 50):
+
+    model.eval()
+        
+    for j, (source_ids, source_lengths, target_ids, target_lengths) in enumerate(data_loader):
+        if not attention:
+            result = decoder(model, source_ids.to(device), source_lengths.to(device), max_len = max_len)
+        else:
+            result, _ = decoder(model, source_ids.to(device), source_lengths.to(device), max_len = max_len)
+
+        # Remove <s>
+        source_ids = source_ids[0, 1:]
+        target_ids = target_ids[0, 1:]
+
+        # Remove </s> and <pad>
+        source_ids = source_ids[:np.where(source_ids == EOS_INDEX)[0][0]]
+        target_ids = target_ids[:np.where(target_ids == EOS_INDEX)[0][0]]
+
+        prediction = " ".join(utils.lookup_words(result, vocab = target_vocab))
+        target = " ".join(utils.lookup_words(target_ids, vocab = target_vocab))
+   
+        print("Example {}".format(j + 1))
+        print("Source : ", " ".join(lookup_words(source_ids, vocab = source_vocab)))
+        print("Target : ", " ".join(lookup_words(target_ids, vocab = target_vocab)))
+        print("Prediction: ", " ".join(lookup_words(result, vocab = target_vocab)))
+        print()
+        
+        if j == n - 1:
+            break
